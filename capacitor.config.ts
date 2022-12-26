7fa1a86a18398af5d506f49a2ea9f960e8eec754
Victor Manuel Soesilo <79180895+victorman01@users.noreply.github.com>
2022-12-26 22:57:01 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'project_uas_hybrid',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
